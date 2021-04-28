3617323766120e79c45f285c8c58c9a115d56030
Sevod <ssevod@gmail.com>
2021-04-28 20:47:28 +0300

    parse one line

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -12,29 +12,100 @@
 
 #include "minishell.h"
 
+int	ft_str_len_space(char *line)
+{
+	int	count;
+
+	count = 0;
+	while (*line && *line != ' ')
+	{
+		line++;
+		count++;
+	}
+	return (count);
+}
+
+void ft_go_end_space(char **line)
+{
+	if (**line && **line == ' ')
+		(*line)++;
+}
+
+int	ft_count_str(char *line)
+{
+	int count;
+
+	ft_go_end_space(&line);
+	if (*line)
+		count = 1;
+	else
+		return (0);
+	while (*line)
+	{
+		if (*line == ' ')
+		{
+			ft_go_end_space(&line);
+			count++;
+		}
+		line++;
+	}
+	return count;
+}
+
+char** ft_parse_strings(char *line)
+{
+	char **arr_strings;
+	int count_lines;
+	int i;
+	int j;
+
+	count_lines = ft_count_str(line);
+	arr_strings = (char **) malloc(sizeof (char **) * (count_lines + 1));
+	ft_go_end_space(&line);
+	i = 0;
+	while (i < count_lines)
+	{
+		j = 0;
+		arr_strings[i] = (char *) malloc(sizeof (char *) * (ft_str_len_space(line) + 1));
+		while (*line) {
+			arr_strings[i][j] = *line;
+			if (*line == ' ' || !line) {
+				ft_go_end_space(&line);
+				break;
+			}
+			j++;
+			line++;
+		}
+		arr_strings[i][j] = '\0';
+		i++;
+	}
+	arr_strings[i] = NULL;
+	return (arr_strings);
+}
+
 int	ft_parse(char *line, char *home)
 {
-	char	**splt;
+	char	**strs_cmd;
 	int		splt_len;
 
-	splt = ft_split(line, ' ');
-	splt_len = ft_strlen(splt[0]);
-	if (!ft_strncmp(splt[0], "pwd", ft_strlen(splt[0])) && splt_len != 0)
+	strs_cmd = ft_parse_strings(line);
+	splt_len = ft_strlen(strs_cmd[0]);
+	if (!ft_strncmp(strs_cmd[0], "pwd", ft_strlen(strs_cmd[0])) && splt_len != 0)
 		ft_pwd();
-	else if (!ft_strncmp(splt[0], "echo", ft_strlen(splt[0])) && splt_len != 0)
-		ft_echo(splt[1], 1);
-	else if (!ft_strncmp(splt[0], "cd", ft_strlen(splt[0])) && splt_len != 0)
-		ft_cd(splt[1]);
-	else if (!ft_strncmp(splt[0], "exit", ft_strlen(splt[0])) && splt_len != 0)
+	else if (!ft_strncmp(strs_cmd[0], "echo", ft_strlen(strs_cmd[0])) && splt_len != 0)
+		ft_echo(strs_cmd[1], 1);
+	else if (!ft_strncmp(strs_cmd[0], "cd", ft_strlen(strs_cmd[0])) && splt_len != 0)
+		ft_cd(strs_cmd[1]);
+	else if (!ft_strncmp(strs_cmd[0], "exit", ft_strlen(strs_cmd[0])) && splt_len != 0)
 		ft_exit();
-	else if (!ft_strncmp(splt[0], "$?", ft_strlen(splt[0])) && splt_len != 0)
+	else if (!ft_strncmp(strs_cmd[0], "$?", ft_strlen(strs_cmd[0])) && splt_len != 0)
 	{
 		printf("minishell: %d: command not found\n", g_error);
 		g_error = 127;//sergey 27/04/2021
 	}
 	else if (*line != '\3')
-		ft_commands(splt);
+		ft_commands(strs_cmd);
 	ft_write_history(line, home);
-	free_text(splt, ft_maslen(splt));
+	free_text(strs_cmd, ft_maslen(strs_cmd));
 	return (0);
 }
