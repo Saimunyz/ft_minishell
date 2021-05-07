319b01d7cd0d762f530e710faf87384517992bd6
Sevod <ssevod@gmail.com>
2021-05-07 20:44:29 +0300

    fix echo

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -12,17 +12,42 @@
 
 #include "minishell.h"
 
-/*
- * flag or 0 or 1.
- */
-void	ft_echo(char *str, int flag)
+void	ft_echo_write(char *str, int space)
 {
 	g_error = 0;
+	if (space)
+		write(1, " ", 1);
 	while (str && *str && *str != '\n')
 	{
 		write(1, str, 1);
 		str++;
 	}
+}
+
+void	ft_echo(char **str)
+{
+	int i;
+	int	flag;
+	int space;
+
+	g_error = 0;
+	i = 1;
+	flag = 1;
+	space = 0;
+	if (str[1])
+	{
+		if (!ft_strncmp(str[1], "-n", ft_strlen(str[1])))
+		{
+			flag = 0;
+			i = 2;
+		}
+		while (str[i]) {
+			ft_echo_write(str[i], space);
+			i++;
+			space = 1;
+		}
+	}
 	if (flag)
 		write(1, "\n", 1);
 }
+
