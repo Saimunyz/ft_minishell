ea52053e84568532b20e396d9b21cc59b8198f2e
Sevod <ssevod@gmail.com>
2021-08-27 19:37:17 +0300

    norm ft_commands4.c

--- srcs/ft_commands/ft_commands4.c
+++ srcs/ft_commands/ft_commands4.c
@@ -1,12 +1,12 @@
 #include "minishell.h"
 
-void ft_clear_arr(char **arr)
+void	ft_clear_arr(char **arr)
 {
-	char *tmp;
-	char **tmp_arr;
+	char	*tmp;
+	char	**tmp_arr;
 
 	if (!arr)
-		return;
+		return ;
 	tmp_arr = arr;
 	while (*arr)
 	{
@@ -17,24 +17,23 @@ void ft_clear_arr(char **arr)
 	free(tmp_arr);
 }
 
-char *ft_find_aur_command(char *command)
+char	*ft_find_aur_command(char *command)
 {
-	int splt_len;
+	int	splt_len;
 
 	splt_len = ft_strlen(command);
 	if (!ft_strncmp(command, "pwd", ft_strlen(command)) && splt_len != 0)
-		return ft_strdup("pwd");
+		return (ft_strdup("pwd"));
 	else if (!ft_strncmp(command, "echo", ft_strlen(command)) && splt_len != 0)
-		return ft_strdup("echo");
+		return (ft_strdup("echo"));
 	else if (!ft_strncmp(command, "cd", ft_strlen(command)) && splt_len != 0)
-		return "cd";
-		//else if (!ft_strncmp(command, "exit", ft_strlen(command)) && splt_len != 0)
-		//	return ft_strdup("exit");
+		return ("cd");
 	else if (!ft_strncmp(command, "env", ft_strlen(command)) && splt_len != 0)
-		return ft_strdup("env");
-	else if (!ft_strncmp(command, "export", ft_strlen(command)) && splt_len != 0)
-		return "export";
+		return (ft_strdup("env"));
+	else if (!ft_strncmp(command, "export", \
+		ft_strlen(command)) && splt_len != 0)
+		return ("export");
 	else if (!ft_strncmp(command, "unset", ft_strlen(command)) && splt_len != 0)
-		return "unset";
+		return ("unset");
 	return (0);
 }
